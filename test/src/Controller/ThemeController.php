40ae18a5cde898d6270f85dace3308b5ff625ccc
Abhishek <abhishek7620.ak@gmail.com>
2019-01-06 19:45:07 +0530

    First Commit

---
+++ test/src/Controller/ThemeController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\controller;
+
+use App\Controller\AppController;
+
+
+
+class ThemeController extends AppController{
+	console.Log("ThemeController inside")
+	
+
+		public function initialize(){
+			parent::initialize();
+		}
+		public function index(){
+
+		
+		}
+}
+
+
+?>
